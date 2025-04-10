77d6e84c8358f98db51a8b96fb7257ab60a782a9
swawiye <sovereign.wawiye@gmail.com>
2025-04-11 00:26:36 +0300

    styling & functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+//Calculates it
+function display(value) {
+    document.getElementById("calculateBtn").value += value;
+};
+
+//Calculates it
+function calculate() {
+    document.getElementById("calculateBtn").value = "";
+};
+
+//Clearing the screen
+function clearScreen() {
+    document.getElementById("clearBtn").value = "";
+};
