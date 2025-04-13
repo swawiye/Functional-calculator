076e4fe28b935a8516ea78ebfd61969d8622b4fe
swawiye <sovereign.wawiye@gmail.com>
2025-04-14 00:07:14 +0300

    calulate function

--- script.js
+++ script.js
@@ -12,16 +12,19 @@ function clearScreen() {
 
 //Calculation
 function calculate() {
-    //'expr' is the string on the calc screen (ex: "5 + 5")
+    //'expr' is the string on the calc screen (ex: "5 + 10")
     const expr = display.value; 
 
     // '.match(...)' -tries to find parts of the string that fit the pattern inside the parentheses
     const match = expr.match(/^(\d+)([+\-*/])(\d+)$/);
-    // '^' -start of the string
-    // '(\d+)' -first number: one or more digits (ex: "12") — captured as group 1
-    // ([+\-*/])   Operator: one character, either +, -, *, or / — captured as group 2
-    // (\d+)    Second number: one or more digits (e.g., "3") — captured as group 3
-    // $    End of the string
+    // '^' -Start of the string
+    // '(\d+)' -First number: one or more digits (ex: "5") — captured as 'group 1'
+    // '([+\-*/])' -Operator: one character, either +, -, *, or / — captured as 'group 2'
+    // '(\d+)' -Second number: one or more digits (ex: "10") — captured as 'group 3'
+    // '$' -End of the string
+    // '/' -Encloses the expression
+
+    // If the pattern matches, it returns an array of the matched pieces. If not, it returns null.
     if (!match) {
         display.value = "Error";
         return;
@@ -35,6 +38,11 @@ function calculate() {
         '*': (x, y) => x * y,
         '/': (x, y) => y !== 0 ? x / y : "Error" //if y is not 0 the operation is completed otherwise it is an error
     };
+    
+    // If operation exists, then call it with a and b, otherwise set the display to "Error".
+    const operation = operations[operator];
+    display.value = operation ? operation(a, b) : "Error";
+
 }
 
 
