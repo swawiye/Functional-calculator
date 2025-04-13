6e9d50ba1d982d97bdc2af7dc95ed73a618750f9
swawiye <sovereign.wawiye@gmail.com>
2025-04-13 23:54:01 +0300

    calculate function

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-// Display clicked value- append the input to the calculator screen
+// Append the input to the calculator screen
 const display = document.getElementById("calc-screen");
 function appendToDisplay(input) {
     display.value += input;
 };
 
-//Calculates the result
-function calculate() {
-    const screen = document.getElementById("calc-screen");
-    const addBtn = document.getElementById("addBtn");
-    const subBtn = document.getElementById("subBtn");
-    const prodBtn = document.getElementById("prodBtn");
-    const divBtn = document.getElementById("divBtn");
-
-    addBtn.addEventListener('click', function () {
-        
-    })
-}
-
 // Clearing the screen
 function clearScreen() {
-    document.getElementById("calc-screen").value = "";
+    //document.getElementById("calc-screen").value = "";
+    display.value = "";
 };
 
+//Calculation
+function calculate() {
+    //'expr' is the string on the calc screen (ex: "5 + 5")
+    const expr = display.value; 
 
-
+    // '.match(...)' -tries to find parts of the string that fit the pattern inside the parentheses
+    const match = expr.match(/^(\d+)([+\-*/])(\d+)$/);
+    // '^' -start of the string
+    // '(\d+)' -first number: one or more digits (ex: "12") — captured as group 1
+    // ([+\-*/])   Operator: one character, either +, -, *, or / — captured as group 2
+    // (\d+)    Second number: one or more digits (e.g., "3") — captured as group 3
+    // $    End of the string
+    if (!match) {
+        display.value = "Error";
+        return;
+    }
+    const a = parseFloat(match[1]);//converts the string to a floating-point number
+    const operator = match[2];
+    const b = parseFloat(match[3]);
+    const operations = {
+        '+': (x, y) => x + y,
+        '-': (x, y) => x - y,
+        '*': (x, y) => x * y,
+        '/': (x, y) => y !== 0 ? x / y : "Error" //if y is not 0 the operation is completed otherwise it is an error
+    };
+}
 
 
 
